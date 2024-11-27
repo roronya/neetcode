d3d463843f5cb44de6129fcc5852369223f7b178
roronya <roronya628@gmail.com>
2024-11-27 20:45:35 +0900

    add optimal valid anagram

--- valid_anagram/src/Solution.java
+++ valid_anagram/src/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
 
-  public boolean isAnagram(String s, String t) {
+  public boolean myIsAnagram(String s, String t) {
     if (s.length() != t.length()) return false;
     java.util.HashMap<Character, Integer> map = new java.util.HashMap<>();
     for (Character ch: s.toCharArray()) {
@@ -13,6 +13,19 @@ public class Solution {
         return false;
       }
       map.put(ch, count - 1);
+    }
+    return true;
+  }
+
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) return false;
+    int[] count = new int[26];
+    for (int i=0;i<s.length();i++) {
+      count[s.charAt(i) - 'a']++;
+      count[t.charAt(i) - 'a']--;
+    }
+    for (int c: count) {
+      if (c != 0) return false;
     }
     return true;
   }
