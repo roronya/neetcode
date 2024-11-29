468b0eadc3e1435f23cb366871110d8045438da7
roronya <roronya628@gmail.com>
2024-11-29 15:06:17 +0900

    varias two sum

--- arrays_and_hashing_two_sum/src/Solution.java
+++ arrays_and_hashing_two_sum/src/Solution.java
@@ -1,10 +1,33 @@
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum_n2(int[] nums, int target) {
         for (int i=0;i<nums.length;i++) {
             for (int j=i+1;j<nums.length;j++) {
                 if (nums[i] + nums[j] == target) return new int[]{i, j};
             }
         }
         return new int[]{0, 0};
+    }
+
+    public int[] twoSum_twoPath(int[] nums, int target) {
+        var map = new java.util.HashMap<Integer, Integer>();
+        for (int i=0;i<nums.length;i++) {
+            map.put(nums[i], i);
+        }
+        for (int i=0;i<nums.length;i++) {
+            var j = map.get(target-nums[i]);
+            if (j != null && i != j) return new int[]{i, j};
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        var map = new java.util.HashMap<Integer, Integer>();
+        for (int i=0;i<nums.length;i++) {
+            var diff = target - nums[i];
+            var j = map.get((Integer)diff);
+            if (j != null) return new int[] {j, i};
+            map.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
