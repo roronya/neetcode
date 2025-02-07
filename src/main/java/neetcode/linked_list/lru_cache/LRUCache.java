a49b91a348353f79dfd5f944ca1f5e61ef51fab3
roronya <roronya628@gmail.com>
2025-02-07 15:40:55 +0900

    [LRUCache] init

---
+++ src/main/java/neetcode/linked_list/lru_cache/LRUCache.java
@@ -0,0 +1,17 @@
+package neetcode.linked_list.lru_cache;
+
+class LRUCache {
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
---
+++ src/test/java/neetcode/linked_list/lru_cache/LRUCacheTest.java
@@ -0,0 +1,17 @@
+package neetcode.linked_list.lru_cache;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LRUCacheTest {
+    @Test
+    void example() {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 10);  // cache: {1=10}
+        Assertions.assertEquals(10, lRUCache.get(1));
+        lRUCache.put(2, 20);  // cache: {1=10, 2=20}
+        lRUCache.put(3, 30);  // cache: {2=20, 3=30}, key=1 was evicted
+        Assertions.assertEquals(20, lRUCache.get(2));
+        Assertions.assertEquals(-1, lRUCache.get(1));
+    }
+}
