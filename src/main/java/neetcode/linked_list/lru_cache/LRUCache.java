47af640bc4ff9aa83f5c5fc9d8588764c0a3da7a
roronya <roronya628@gmail.com>
2025-02-12 20:34:41 +0900

    [LRUCache] add naive solution

--- src/main/java/neetcode/linked_list/lru_cache/LRUCache.java
+++ src/main/java/neetcode/linked_list/lru_cache/LRUCache.java
@@ -1,17 +1,64 @@
 package neetcode.linked_list.lru_cache;
 
+import neetcode.linked_list.ListNode;
+
+import java.util.HashMap;
+
+
 class LRUCache {
     private int capacity;
+    private int size = 0;
+    private ListNode head;
+    private HashMap<Integer, ListNode> map = new HashMap<>();
+    private HashMap<ListNode, Integer> reverseMap = new HashMap<>();
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        return 0;
+        ListNode node = map.getOrDefault(key, null);
+        return node == null ? -1 : node.val;
     }
 
+    /**
+     * 渡されたkeyでNodeを作る
+     * 内部のLinkedListのheadにする
+     * capacityよりLinkedListのsizeがデカくなったらtailを切り離す
+     * また内部的にHashMapを持っておき、key:Nodeで保存しておく
+     * 切り離されたときにkeyを消しておく
+     *
+     * @param key
+     * @return
+     */
     public void put(int key, int value) {
+        ListNode newNode = new ListNode(value);
+        if (size == 0) {
+            head = newNode;
+            size++;
+            map.put(key, newNode);
+            reverseMap.put(newNode, key);
+            return;
+        }
 
+        if (map.get(key) != null) {
+            map.put(key, newNode);
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+        size++;
+        map.put(key, newNode);
+        reverseMap.put(newNode, key);
+        if (size > capacity) {
+            ListNode current = head;
+            for (int i = 0; i < capacity - 1; i++)
+                current = current.next;
+            int tailKey = reverseMap.get(current.next);
+            reverseMap.remove(current.next);
+            map.remove(tailKey);
+            current.next = null;
+            size--;
+        }
     }
 }
--- src/test/java/neetcode/linked_list/lru_cache/LRUCacheTest.java
+++ src/test/java/neetcode/linked_list/lru_cache/LRUCacheTest.java
@@ -14,4 +14,16 @@ class LRUCacheTest {
         Assertions.assertEquals(20, lRUCache.get(2));
         Assertions.assertEquals(-1, lRUCache.get(1));
     }
+
+    @Test
+    void example1() {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 10);  // cache: {1=10}
+        Assertions.assertEquals(10, lRUCache.get(1));
+        lRUCache.put(1, 20);  // cache: {1=20}
+        Assertions.assertEquals(20, lRUCache.get(1));
+        lRUCache.put(1, 30);  // cache: {1=20}
+        Assertions.assertEquals(30, lRUCache.get(1));
+    }
+
 }
