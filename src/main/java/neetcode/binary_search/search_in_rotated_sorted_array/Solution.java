07cf06add78a68265a3c60f60455cc85993f2d2a
roronya <roronya628@gmail.com>
2025-02-25 23:06:46 +0900

    [Find Minumum in Rotated Sorted Array] solved

--- src/main/java/neetcode/binary_search/search_in_rotated_sorted_array/Solution.java
+++ src/main/java/neetcode/binary_search/search_in_rotated_sorted_array/Solution.java
@@ -2,20 +2,53 @@ package neetcode.binary_search.search_in_rotated_sorted_array;
 
 public class Solution {
     int search(int[] nums, int target) {
-        return mySearch(nums, target);
+        return binarySearch(nums, target);
+    }
+
+    int binarySearch(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target) return m;
+
+            if (nums[l] <= nums[m]) {
+                if (nums[m] < target || target < nums[l]) l = m + 1;
+                else r = m - 1;
+            } else { // nums[m] <= nums[r]
+                if (target < nums[m] || nums[r] < target) r = m - 1;
+                else l = m + 1;
+            }
+        }
+        return -1;
     }
 
     /**
-     int[] nums = {4,5,6,7,0,1,2};
+     * t=1
+     * [5,6,0,1,2,3,4] l>t  && t<m
+     * [1,2,3,4,5] l<=t && t<m
+     * [4,5,1,2,3] l<=t && t<m
+     * [5,1,2,3,4] t<r<m
      */
     int mySearch(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
-        while(l <= r) {
+        while (l <= r) {
             System.out.println(l + ":" + r);
             int m = (l + r) / 2;
             if (nums[m] == target) return m;
-            if (nums[l] < nums[m] && nums[l] <= target && target < nums[m]) r = m - 1;
-            else l = m + 1;
+            if (nums[l] <= nums[r]) {
+                if (nums[m] < target) l = m + 1;
+                else r = m - 1;
+            } else {
+                if (target < nums[m]) {
+                    if (nums[l] <= target) r = m - 1; // l <= t < m
+                    else l = m + 1; // target < nums[m] < nums[l] -> m < t <= r
+                } else { // nums[m] < target
+                    if (target <= nums[r]) l = m + 1;// m < t <= r
+                    else r = m - 1; // m < r < t -> l<= t
+                }
+            }
         }
         return -1;
     }
--- src/test/java/neetcode/binary_search/search_in_rotated_sorted_array/SolutionTest.java
+++ src/test/java/neetcode/binary_search/search_in_rotated_sorted_array/SolutionTest.java
@@ -7,7 +7,7 @@ class SolutionTest {
     @Test
     void example1() {
         Solution solution = new Solution();
-        int[] nums = {3,4,5,6,1,2};
+        int[] nums = {3, 4, 5, 6, 1, 2};
         int target = 1;
 
         int actual = solution.search(nums, target);
@@ -17,7 +17,7 @@ class SolutionTest {
     @Test
     void example2() {
         Solution solution = new Solution();
-        int[] nums = {3,5,6,0,1,2};
+        int[] nums = {3, 5, 6, 0, 1, 2};
         int target = 4;
 
         int actual = solution.search(nums, target);
@@ -27,7 +27,7 @@ class SolutionTest {
     @Test
     void example3() {
         Solution solution = new Solution();
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
 
         int actual = solution.search(nums, target);
@@ -37,7 +37,7 @@ class SolutionTest {
     @Test
     void example4() {
         Solution solution = new Solution();
-        int[] nums = {5,1,3};
+        int[] nums = {5, 1, 3};
         int target = 5;
 
         int actual = solution.search(nums, target);
@@ -47,8 +47,18 @@ class SolutionTest {
     @Test
     void example5() {
         Solution solution = new Solution();
-        int[] nums = {5,1,2,3,4};
+        int[] nums = {5, 1, 2, 3, 4};
         int target = 5;
+
+        int actual = solution.search(nums, target);
+        Assertions.assertEquals(0, actual);
+    }
+
+    @Test
+    void example6() {
+        Solution solution = new Solution();
+        int[] nums = {5, 1, 2, 3, 4};
+        int target = 1;
 
         int actual = solution.search(nums, target);
         Assertions.assertEquals(1, actual);
