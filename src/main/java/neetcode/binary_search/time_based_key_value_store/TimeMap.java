5e1a1dc9af0219e8c8260bd9d92429e0bf37cad8
roronya <roronya628@gmail.com>
2025-02-27 08:11:18 +0900

    [Time Based Key-Value Store] add naive solution

--- src/main/java/neetcode/binary_search/time_based_key_value_store/TimeMap.java
+++ src/main/java/neetcode/binary_search/time_based_key_value_store/TimeMap.java
@@ -19,16 +19,20 @@ class TimeMap {
         if (map.containsKey(key)) {
             values = map.get(key);
         } else {
-            values = new String[1001];
+            values = new String[2001];
         }
-        values[timestamp] = value;
+        while (timestamp <= 1000) {
+            values[timestamp] = value;
+            timestamp++;
+        }
         map.put(key, values);
     }
 
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return null;
-        String[] values = map.get(key);
-        return searchMax(values, timestamp);
+        if (!map.containsKey(key)) return "";
+        //System.out.println(Arrays.toString(map.get(key)));
+        String value = map.get(key)[timestamp];
+        return Objects.isNull(value) ? "" : value;
     }
 
     /**
@@ -39,21 +43,21 @@ class TimeMap {
      * @return
      */
     String searchMax(String[] values, int index) {
-        // System.out.println(Arrays.toString(values));
         int l = 1;
-        int r = 1000;
+        int r = index;
         int max = 0; // values[0] must be always null.
         while (l <= r) {
             System.out.println(l + ":" + r);
             int m = (l + r) / 2;
             if (Objects.isNull(values[m])) {
-                r = m - 1;
+                l = m + 1;
             } else {
                 max = Math.max(max, m);
-                l = m + 1;
+                r = m - 1;
             }
         }
-        return values[max];
+        System.out.println(values[max]);
+        return Objects.isNull(values[max]) ? "" : values[max];
     }
 }
 
--- src/test/java/neetcode/binary_search/time_based_key_value_store/TimeMapTest.java
+++ src/test/java/neetcode/binary_search/time_based_key_value_store/TimeMapTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class TimeMapTest {
     @Test
-    void exmaple1() {
+    void example1() {
         TimeMap timeMap = new TimeMap();
         timeMap.set("alice", "happy", 1);  // store the key "alice" and value "happy" along with timestamp = 1.
         Assertions.assertEquals("happy", timeMap.get("alice", 1));           // return "happy"
@@ -15,7 +15,7 @@ class TimeMapTest {
     }
 
     @Test
-    void example1() {
+    void example2() {
         TimeMap timeMap = new TimeMap();
         timeMap.set("foo", "bar", 1);
         String value = timeMap.get("foo", 1);
@@ -23,9 +23,22 @@ class TimeMapTest {
         timeMap.get("foo", 3);
         Assertions.assertEquals("bar", value);
         timeMap.set("foo", "bar2", 4);
-        timeMap.get("foo", 4);
+        value = timeMap.get("foo", 4);
         Assertions.assertEquals("bar2", value);
-        timeMap.get("foo", 5);
+        value = timeMap.get("foo", 5);
         Assertions.assertEquals("bar2", value);
     }
+
+    @Test
+    void example3() {
+        TimeMap timeMap = new TimeMap();
+        timeMap.set("key1", "value1", 10);
+        String value = timeMap.get("key1", 1);
+        Assertions.assertEquals("", value);
+        value = timeMap.get("key1", 10);
+        Assertions.assertEquals("value1", value);
+        value = timeMap.get("key1", 11);
+        Assertions.assertEquals("value1", value);
+    }
+
 }
