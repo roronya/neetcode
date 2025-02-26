96bf40d9100f1fbad832df543e1de073bc9bd296
roronya <roronya628@gmail.com>
2025-02-27 08:42:01 +0900

    [Time Based Key-Value Store] add binary search solution

--- src/main/java/neetcode/binary_search/time_based_key_value_store/TimeMap.java
+++ src/main/java/neetcode/binary_search/time_based_key_value_store/TimeMap.java
@@ -1,63 +1,65 @@
 package neetcode.binary_search.time_based_key_value_store;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.List;
 
 /**
  * 1 <= key.length, value.length <= 100
  * key and value only include lowercase English letters and digits.
  * 1 <= timestamp <= 1000
+ * You should aim for a solution with
+ * O(1) time for set(),
+ * O(logn) time for get(),
+ * and O(m * n) space,
+ * where n is the total number of values associated with a key,
+ * and m is the total number of keys.
  */
 class TimeMap {
-    HashMap<String, String[]> map = new HashMap<>();
+    HashMap<String, List<Element>> map = new HashMap<>();
+
+    class Element {
+        String value;
+        int timestamp;
+
+        public Element(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
 
     public TimeMap() {
     }
 
     public void set(String key, String value, int timestamp) {
-        String[] values;
+        List<Element> elements;
         if (map.containsKey(key)) {
-            values = map.get(key);
+            elements = map.get(key);
         } else {
-            values = new String[2001];
+            elements = new ArrayList<>();
         }
-        while (timestamp <= 1000) {
-            values[timestamp] = value;
-            timestamp++;
-        }
-        map.put(key, values);
+        Element element = new Element(value, timestamp);
+        elements.add(element);
+        map.put(key, elements);
     }
 
     public String get(String key, int timestamp) {
         if (!map.containsKey(key)) return "";
-        //System.out.println(Arrays.toString(map.get(key)));
-        String value = map.get(key)[timestamp];
-        return Objects.isNull(value) ? "" : value;
-    }
-
-    /**
-     * search max value from values until index
-     *
-     * @param values
-     * @param index
-     * @return
-     */
-    String searchMax(String[] values, int index) {
-        int l = 1;
-        int r = index;
-        int max = 0; // values[0] must be always null.
+        List<Element> elements = map.get(key);
+        int l = 0, r = elements.size() - 1;
+        Element latest = new Element("", -1);
         while (l <= r) {
-            System.out.println(l + ":" + r);
+            System.out.print(l + ":" + r);
             int m = (l + r) / 2;
-            if (Objects.isNull(values[m])) {
+            Element element = elements.get(m);
+            if (element.timestamp <= timestamp) {
+                latest = latest.timestamp < element.timestamp ? element : latest;
                 l = m + 1;
             } else {
-                max = Math.max(max, m);
                 r = m - 1;
             }
         }
-        System.out.println(values[max]);
-        return Objects.isNull(values[max]) ? "" : values[max];
+        return latest.timestamp < 0 ? "" : latest.value;
     }
 }
 
