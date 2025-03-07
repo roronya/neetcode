4f4d36d471efaa11747735654b7c65b467592a69
roronya <roronya628@gmail.com>
2025-03-07 11:10:04 +0900

    [Same Binary Tree] add

---
+++ src/test/java/neetcode/trees/same_binary_tree/Solution.java
@@ -0,0 +1,9 @@
+package neetcode.trees.same_binary_tree;
+
+import neetcode.trees.TreeNode;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return true;
+    }
+}
---
+++ src/test/java/neetcode/trees/same_binary_tree/SolutionTest.java
@@ -0,0 +1,42 @@
+package neetcode.trees.same_binary_tree;
+
+import neetcode.trees.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void example1() {
+        TreeNode p =
+                new TreeNode(1,
+                        new TreeNode(2),
+                        new TreeNode(3));
+        TreeNode q =
+                new TreeNode(1,
+                        new TreeNode(2),
+                        new TreeNode(3));
+        Solution solution = new Solution();
+        boolean actual = solution.isSameTree(p, q);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void example2() {
+        TreeNode p =
+                new TreeNode(
+                        4,
+                        new TreeNode(7),
+                        null
+                );
+        TreeNode q =
+                new TreeNode(
+                        4,
+                        null,
+                        new TreeNode(7)
+                );
+        Solution solution = new Solution();
+        boolean actual = solution.isSameTree(p, q);
+        Assertions.assertTrue(actual);
+    }
+
+}
