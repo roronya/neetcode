824505e2605c1346949fea3e3535e2d0076c48d8
roronya <roronya628@gmail.com>
2025-03-13 10:55:24 +0900

    [Binary Tree Right Side View] add mySolution

--- src/main/java/neetcode/trees/binary_tree_right_side_view/Solution.java
+++ src/main/java/neetcode/trees/binary_tree_right_side_view/Solution.java
@@ -2,11 +2,14 @@ package neetcode.trees.binary_tree_right_side_view;
 
 import neetcode.trees.TreeNode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-       return mySolution(root);
+        return mySolution(root);
     }
 
     /**
@@ -14,24 +17,24 @@ class Solution {
      * siblingの中で最も右にあるやつ
      * 各レベルで最も右にあるやつ(stackのtop)をres.addする
      * 次の階層のnodeを左からstackに積む
-     * time complexity: O(h * 2h) = O(h)
-     * space complexity: O(2h)
+     * time complexity: O(n)
+     * space complexity: O(n)
      * h is size of last layer of tree
      */
     public List<Integer> mySolution(TreeNode root) {
         if (root == null) return Collections.emptyList();
+        List<Integer> ans = new ArrayList<>();
         LinkedList<TreeNode> list = new LinkedList<>();
         list.addLast(root);
-
-        List<Integer> res = new ArrayList<>();
-        while(!stack.isEmpty()) {
-            TreeNode top = stack.pop();
-            if (top != null) res.add(top.val);
-
-            stack = new Stack<>();
-            if (top.right != null) stack.add(top.right);
-            else if (top.left != null) stack.add(top.left);
+        while (!list.isEmpty()) {
+            ans.add(list.getLast().val);
+            LinkedList<TreeNode> newList = new LinkedList<>();
+            for (TreeNode node : list) {
+                if (node.left != null) newList.addLast(node.left);
+                if (node.right != null) newList.addLast(node.right);
+            }
+            list = newList;
         }
-        return res;
+        return ans;
     }
 }
