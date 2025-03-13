33da0f6a89bef2e299dade4d12d58e0ff23341ac
roronya <roronya628@gmail.com>
2025-03-13 11:00:58 +0900

    [Binary Tree Right Side View] add dfs

--- src/main/java/neetcode/trees/binary_tree_right_side_view/Solution.java
+++ src/main/java/neetcode/trees/binary_tree_right_side_view/Solution.java
@@ -9,7 +9,23 @@ import java.util.List;
 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        return mySolution(root);
+        return depthFirstSearch(root);
+    }
+
+    /**
+     * 右側から順に調べていって、各レイヤで一番右のやつを見つけたらそれをいれる
+     */
+    public List<Integer> depthFirstSearch(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, 0, ans);
+        return ans;
+    }
+
+    void dfs(TreeNode node, int depth, List<Integer> ans) {
+        if (node == null) return;
+        if (depth == ans.size()) ans.add(node.val);
+        dfs(node.right, depth + 1, ans);
+        dfs(node.left, depth + 1, ans);
     }
 
     /**
