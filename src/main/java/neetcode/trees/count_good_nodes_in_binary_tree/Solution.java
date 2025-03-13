4f5fc3ecc6ab2f19fe5b0e3c90ebe2d68e33b6f5
roronya <roronya628@gmail.com>
2025-03-13 18:27:45 +0900

    [Count Good Nodes in Binary Tree] add

---
+++ src/main/java/neetcode/trees/count_good_nodes_in_binary_tree/Solution.java
@@ -0,0 +1,9 @@
+package neetcode.trees.count_good_nodes_in_binary_tree;
+
+import neetcode.trees.TreeNode;
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return 0;
+    }
+}
