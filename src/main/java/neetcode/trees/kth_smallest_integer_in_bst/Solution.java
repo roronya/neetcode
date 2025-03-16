8b2dca7961533bec91874a44e2ca27bf93c893a7
roronya <roronya628@gmail.com>
2025-03-17 00:01:20 +0900

    [Kth Smallest Integer in BST] add

---
+++ src/main/java/neetcode/trees/kth_smallest_integer_in_bst/Solution.java
@@ -0,0 +1,10 @@
+package neetcode.trees.kth_smallest_integer_in_bst;
+
+import neetcode.trees.TreeNode;
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        return 0;
+    }
+}
+
---
+++ src/test/java/neetcode/trees/kth_smallest_integer_in_bst/SolutionTest.java
@@ -0,0 +1,34 @@
+package neetcode.trees.kth_smallest_integer_in_bst;
+
+import neetcode.trees.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void example1() {
+        TreeNode root =
+                new TreeNode(2,
+                        new TreeNode(1),
+                        new TreeNode(3));
+        int k = 1;
+        Solution solution = new Solution();
+        int actual = solution.kthSmallest(root, k);
+        Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    void example2() {
+        // Input: root = [4,3,5,2,null], k = 4
+        TreeNode root =
+                new TreeNode(4,
+                        new TreeNode(3,
+                                new TreeNode(2),
+                                null),
+                        new TreeNode(3));
+        int k = 4;
+        Solution solution = new Solution();
+        int actual = solution.kthSmallest(root, k);
+        Assertions.assertEquals(5, actual);
+    }
+}
