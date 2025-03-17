07c7a3abe6f80cf5f33eca4a321841329ebb1771
roronya <roronya628@gmail.com>
2025-03-17 23:53:37 +0900

    [Construct Binary Tree from Preorder and Inorder Traversal] add

---
+++ src/main/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
@@ -0,0 +1,11 @@
+package neetcode.trees.construct_binary_tree_from_preorder_and_inorder_traversal;
+
+
+import neetcode.trees.TreeNode;
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return new TreeNode();
+    }
+}
+
---
+++ src/test/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/SolutionTest.java
@@ -0,0 +1,37 @@
+package neetcode.trees.construct_binary_tree_from_preorder_and_inorder_traversal;
+
+import neetcode.trees.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void example1() {
+        // Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
+        //Output: [1,2,3,null,null,null,4]
+        int[] preorder = {1, 2, 3, 4};
+        int[] inorder = {2, 1, 3, 4};
+        Solution solution = new Solution();
+        TreeNode actual = solution.buildTree(preorder, inorder);
+
+        TreeNode expected =
+                new TreeNode(1,
+                        new TreeNode(2),
+                        new TreeNode(3,
+                                null,
+                                new TreeNode(4)));
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void example2() {
+        int[] preorder = {1};
+        int[] inorder = {1};
+        Solution solution = new Solution();
+        TreeNode actual = solution.buildTree(preorder, inorder);
+
+        TreeNode expected =
+                new TreeNode(1);
+        Assertions.assertEquals(expected, actual);
+    }
+}
