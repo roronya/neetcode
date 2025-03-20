e8572c0e906d513ef27906d0d341c30b04fc7b94
roronya <roronya628@gmail.com>
2025-03-20 21:25:30 +0900

    [Construct Binary Tree from Preorder and Inorder Traversal] optimale

--- src/main/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
+++ src/main/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
@@ -7,7 +7,24 @@ import java.util.Arrays;
 
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return mySolution(preorder, inorder);
+        // return mySolution(preorder, inorder);
+        return dfs(preorder, inorder, Integer.MAX_VALUE);
+    }
+
+    int preIdx = 0;
+    int inIdx = 0;
+
+    TreeNode dfs(int[] preorder, int[] inorder, int limit) {
+        if (preIdx >= preorder.length) return null;
+        if (inorder[inIdx] == limit) {
+            inIdx++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preIdx++]);
+        root.left = dfs(preorder, inorder, root.val);
+        root.right = dfs(preorder, inorder, limit);
+        return root;
     }
 
     /**
@@ -25,6 +42,11 @@ class Solution {
         int m = 0;
         for (m = 0; inorder[m] != root.val; m++) ;
         int[] left = Arrays.copyOfRange(inorder, 0, m);
+        if (left.length > 0) {
+            preorder = Arrays.copyOfRange(preorder, 1, preorder.length);
+            root.left = mySolution(preorder, left);
+        }
+
         int[] right = Arrays.copyOfRange(inorder, m + 1, inorder.length);
         int[] newPreorder = Arrays.copyOfRange(preorder, 1, preorder.length);
         root.left = mySolution(newPreorder, left);
