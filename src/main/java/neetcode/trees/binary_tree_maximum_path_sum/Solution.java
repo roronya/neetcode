1ae498386971692c69e5f9c692f96ad6870ee125
roronya <roronya628@gmail.com>
2025-03-22 15:17:29 +0900

    [Binary Tree Maximum Path Sum] add dfs

--- src/main/java/neetcode/trees/binary_tree_maximum_path_sum/Solution.java
+++ src/main/java/neetcode/trees/binary_tree_maximum_path_sum/Solution.java
@@ -3,7 +3,28 @@ package neetcode.trees.binary_tree_maximum_path_sum;
 import neetcode.trees.TreeNode;
 
 class Solution {
+    int res = Integer.MIN_VALUE;
+
     public int maxPathSum(TreeNode root) {
-        return 0;
+        dfs(root);
+        return res;
+    }
+
+    private int getMax(TreeNode root) {
+        if (root == null) return 0;
+        int left = getMax(root.left);
+        int right = getMax(root.right);
+        int path = root.val + Math.max(left, right);
+        return Math.max(0, path);
+    }
+
+    // O(n^2)
+    private void dfs(TreeNode root) {
+        if (root == null) return;
+        int left = getMax(root.left);
+        int right = getMax(root.right);
+        res = Math.max(res, root.val + left + right);
+        dfs(root.left);
+        dfs(root.right);
     }
 }
