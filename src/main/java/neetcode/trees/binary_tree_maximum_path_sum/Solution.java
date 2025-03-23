2e86f2225a43141b8aa9cdb0b16221dae547f29e
roronya <roronya628@gmail.com>
2025-03-23 10:55:23 +0900

    [Binary Tree Maximum Path Sum] add optimal dfs

--- src/main/java/neetcode/trees/binary_tree_maximum_path_sum/Solution.java
+++ src/main/java/neetcode/trees/binary_tree_maximum_path_sum/Solution.java
@@ -6,7 +6,8 @@ class Solution {
     int res = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
-        dfs(root);
+        // dfs(root);
+        optimalDfs(root);
         return res;
     }
 
@@ -18,7 +19,16 @@ class Solution {
         return Math.max(0, path);
     }
 
-    // O(n^2)
+    /**
+     * leftとrightそれぞれのmax経路を足す
+     * resにその最大値を保存していく
+     * dfsでleftとrightをそれぞれ見ていく
+     * O(n^2)
+     *
+     * O(n)にできるか
+     * 上から調べるからすべてのNodeについてO(n)の探索になってしまう
+     * 下から調べて上に上がっていけば全体でO(n)にできる
+     */
     private void dfs(TreeNode root) {
         if (root == null) return;
         int left = getMax(root.left);
@@ -26,5 +36,21 @@ class Solution {
         res = Math.max(res, root.val + left + right);
         dfs(root.left);
         dfs(root.right);
+    }
+
+    /**
+     *
+     * @param root
+     * @return そのNodeでの最大値(右か左かの最大の方を選んだときの値に自身を足して返す
+     */
+    private int optimalDfs(TreeNode root) {
+        if (root == null) return 0;
+        // leftやrightを選ぶとむしろマイナスになるケースがあるので0と比較する
+        int leftMax = Math.max(optimalDfs(root.left), 0);
+        int rightMax = Math.max(optimalDfs(root.right), 0);
+
+        // 上のNodeではなくこのノードで経路を構築した場合どうかというのをメモしておく
+        res = Math.max(res, root.val + leftMax + rightMax);
+        return root.val + Math.max(leftMax, rightMax);
     }
 }
