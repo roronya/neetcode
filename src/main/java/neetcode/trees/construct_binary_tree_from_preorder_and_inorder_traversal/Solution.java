660462a0891557363388749c3580800939d762e9
roronya <roronya628@gmail.com>
2025-03-23 11:27:33 +0900

    [Construct Binary Tree from Preorder and Inorder Traversal] retry

--- src/main/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
+++ src/main/java/neetcode/trees/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.java
@@ -7,25 +7,10 @@ import java.util.Arrays;
 
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // return mySolution(preorder, inorder);
-        return dfs(preorder, inorder, Integer.MAX_VALUE);
+        return mySolution(preorder, inorder);
     }
 
-    int preIdx = 0;
-    int inIdx = 0;
 
-    TreeNode dfs(int[] preorder, int[] inorder, int limit) {
-        if (preIdx >= preorder.length) return null;
-        if (inorder[inIdx] == limit) {
-            inIdx++;
-            return null;
-        }
-
-        TreeNode root = new TreeNode(preorder[preIdx++]);
-        root.left = dfs(preorder, inorder, root.val);
-        root.right = dfs(preorder, inorder, limit);
-        return root;
-    }
 
     /**
      * preorder= [1,2,4,5,3,6,7]
@@ -36,21 +21,16 @@ class Solution {
      * [4,2,5]の中でpreorderの最もindexが若いのは2なので、2がleftの中のrootになる。 => [4], [5]
      * これを再帰的に繰り返してツリーをrebuildできる。
      */
+    int preIndex = 0;
     TreeNode mySolution(int[] preorder, int[] inorder) {
-        if (preorder.length == 0) return null;
-        TreeNode root = new TreeNode(preorder[0]);
-        int m = 0;
-        for (m = 0; inorder[m] != root.val; m++) ;
-        int[] left = Arrays.copyOfRange(inorder, 0, m);
-        if (left.length > 0) {
-            preorder = Arrays.copyOfRange(preorder, 1, preorder.length);
-            root.left = mySolution(preorder, left);
-        }
-
-        int[] right = Arrays.copyOfRange(inorder, m + 1, inorder.length);
-        int[] newPreorder = Arrays.copyOfRange(preorder, 1, preorder.length);
-        root.left = mySolution(newPreorder, left);
-        root.right = mySolution(newPreorder, right);
+        System.out.println(Arrays.toString(Arrays.copyOfRange(preorder, preIndex, preorder.length)) + ":" + Arrays.toString(inorder));
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        TreeNode root = new TreeNode(preorder[preIndex]);
+        int inorderIndex = 0;
+        while(inorder[inorderIndex] != preorder[preIndex]) inorderIndex++;
+        preIndex++;
+        root.left = mySolution(preorder, Arrays.copyOfRange(inorder, 0, inorderIndex));
+        root.right = mySolution(preorder, Arrays.copyOfRange(inorder, inorderIndex + 1, inorder.length));
         return root;
     }
 }
