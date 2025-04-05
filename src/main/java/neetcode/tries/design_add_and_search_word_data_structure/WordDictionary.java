377a4ebe8f2b5833484b47cd08e5169994ace03a
roronya <roronya628@gmail.com>
2025-04-05 22:34:07 +0900

    [Design Add and Search Word Data Structure] sove

--- src/main/java/neetcode/tries/design_add_and_search_word_data_structure/WordDictionary.java
+++ src/main/java/neetcode/tries/design_add_and_search_word_data_structure/WordDictionary.java
@@ -19,6 +19,9 @@ class WordDictionary {
             }
             cur = cur.children[i];
         }
+        cur.isEnd = true;
+        System.out.println(root);
+        System.out.println(cur);
     }
 
     public boolean search(String word) {
@@ -28,25 +31,43 @@ class WordDictionary {
         for (int i = 0; i < word.length(); i++) {
             Character ch = word.charAt(i);
             Queue<Node> newQ = new LinkedList<>();
+            System.out.println("---------");
+            System.out.println("q: " + q);
             while (!q.isEmpty()) {
                 Node node = q.remove();
+                System.out.println(node);
                 if (ch == '.') {
                     for (Node c : node.children) {
                         if (c != null) newQ.add(c);
                     }
                 } else {
                     int j = ch - 'a';
-                    if (node.children[j] == null) return false;
-                    newQ.add(node.children[j]);
+                    if (node.children[j] != null) newQ.add(node.children[j]);
                 }
             }
+            System.out.println("newQ: " + newQ);
             if (newQ.isEmpty()) return false;
             q = newQ;
         }
-        return true;
+        for (Node n : q) {
+            if (n.isEnd) return true;
+        }
+        return false;
     }
 }
 
 class Node {
     Node[] children = new Node[26];
+    boolean isEnd = false;
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (children[i] != null) {
+                sb.append(Character.toChars('a' + i));
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
