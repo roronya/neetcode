f1ce2fa3e18b980dbb70175b8ef24a1c8ee78991
roronya <roronya628@gmail.com>
2025-04-08 05:43:48 +0900

    [Subsets] solve

--- src/main/java/neetcode/backtracking/subsets/Solution.java
+++ src/main/java/neetcode/backtracking/subsets/Solution.java
@@ -1,9 +1,25 @@
 package neetcode.backtracking.subsets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        return List.of();
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(nums, 0, subset, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset, res);
+        // 先に入れて、あとから抜くのが大事。親ノードに再起が返ったときにきれいな状態になっている必要がある
+        subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset, res);
     }
 }
--- src/test/java/neetcode/backtracking/subsets/SolutionTest.java
+++ src/test/java/neetcode/backtracking/subsets/SolutionTest.java
@@ -12,14 +12,14 @@ class SolutionTest {
         List<List<Integer>> actual = solution.subsets(new int[]{1, 2, 3});
         // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
         List<List<Integer>> expected = List.of(
-                List.of(),
-                List.of(1),
-                List.of(2),
+                List.of(1, 2, 3),
                 List.of(1, 2),
-                List.of(3),
                 List.of(1, 3),
+                List.of(1),
                 List.of(2, 3),
-                List.of(1, 2, 3)
+                List.of(2),
+                List.of(3),
+                List.of()
         );
         Assertions.assertEquals(expected, actual);
     }
@@ -28,7 +28,7 @@ class SolutionTest {
     void exmaple2() {
         Solution solution = new Solution();
         List<List<Integer>> actual = solution.subsets(new int[]{7});
-        List<List<Integer>> expected = List.of(List.of(), List.of(7));
+        List<List<Integer>> expected = List.of(List.of(7), List.of());
         Assertions.assertEquals(expected, actual);
     }
 }
