6765313389b94bbe5cf91dcdb19aa787546c6ddd
roronya <roronya628@gmail.com>
2025-04-10 06:54:00 +0900

    [Combination Sum II] optimal

--- src/main/java/neetcode/backtracking/combination_sum_ii/Solution.java
+++ src/main/java/neetcode/backtracking/combination_sum_ii/Solution.java
@@ -8,8 +8,41 @@ class Solution {
     Set<String> set;
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return myCombinationSum2(candidates, target);
+        //return myCombinationSum2(candidates, target);
+        return optimal(candidates, target);
     }
+
+    public List<List<Integer>> optimal(int[] candidates, int target) {
+        ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(0, new ArrayList<>(), 0, candidates, target);
+        return ans;
+    }
+
+    private void dfs(int idx, List<Integer> path, int cur, int[] candidates, int target) {
+        System.out.println(path);
+        if (cur == target) {
+            ans.add(new ArrayList<>(path));
+        }
+
+        for (int i = idx; i < candidates.length; i++) {
+            // candidateが[1,2,2,3]のとき
+            // [1,2,3]の組み合わせが2つできてしまうのを避けたいので、被ってる数字はスキップする
+            // i <= idxのとき最初の数字を選ぶときで、このときはすでにpathに同じ数字が入っていることは認識していないので
+            // [2,2]の組み合わせを作ることはできる
+            if (i > idx && candidates[i] == candidates[i - 1]) {
+                //
+                continue;
+            }
+            if (cur + candidates[i] > target) {
+                break;
+            }
+            path.add(candidates[i]);
+            dfs(i + 1, path, cur + candidates[i], candidates, target);
+            path.remove(path.size() - 1);
+        }
+    }
+
 
     public List<List<Integer>> myCombinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
