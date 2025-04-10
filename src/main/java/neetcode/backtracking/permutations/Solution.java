73dcea4cc4672a17dade5496839639eec0e586ec
roronya <roronya628@gmail.com>
2025-04-11 06:30:52 +0900

    [Permutations] using mask

--- src/main/java/neetcode/backtracking/permutations/Solution.java
+++ src/main/java/neetcode/backtracking/permutations/Solution.java
@@ -7,7 +7,7 @@ class Solution {
     List<List<Integer>> ans;
 
     public List<List<Integer>> permute(int[] nums) {
-        return bruteforce(nums);
+        return myPermute(nums);
     }
 
     public List<List<Integer>> bruteforce(int[] nums) {
@@ -22,4 +22,45 @@ class Solution {
         }
         return ans;
     }
+
+    public List<List<Integer>> myPermute(int[] nums) {
+        ans = new ArrayList<>();
+        //dfs(nums, new ArrayList<>());
+        backtrack(new ArrayList<>(), nums, new boolean[nums.length]);
+        return ans;
+    }
+
+    // time complexity: O(n * n)
+    // space complexity: O(n)
+    private void dfs(int[] nums, List<Integer> cur) {
+        if (nums.length == cur.size()) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int num : nums) {
+            if (cur.contains(num)) continue;
+            cur.add(num);
+            dfs(nums, cur);
+            cur.remove(cur.size() - 1);
+        }
+    }
+
+    // time complexity: O(n)
+    // space complexity: O(n)
+    private void backtrack(List<Integer> perm, int[] nums, boolean[] pick) {
+        if (perm.size() == nums.length) {
+            ans.add(new ArrayList<>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!pick[i]) {
+                perm.add(nums[i]);
+                pick[i] = true;
+                backtrack(perm, nums, pick);
+                perm.remove(perm.size() - 1);
+                pick[i] = false;
+            }
+        }
+    }
+
 }
