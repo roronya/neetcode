6a5763547ea6e0c004d42f6f0be2fafc80123af2
roronya <roronya628@gmail.com>
2025-04-17 20:44:08 +0900

    [Climbing Stairs] add dp

--- src/main/java/neetcode/one_d_dp/climbing_stairs/Solution.java
+++ src/main/java/neetcode/one_d_dp/climbing_stairs/Solution.java
@@ -3,8 +3,30 @@ package neetcode.one_d_dp.climbing_stairs;
 class Solution {
     private int ans;
 
+    private int[] cache;
+    private int dp(int n) {
+        cache = new int[n];
+        for(int i = 0; i < n; i++) {
+            cache[i] = -1;
+        }
+        return dfs(n, 0);
+    }
+
+    /**
+     * n = 10
+     * i = 9まできたらあとは1を1回足すしかない => 1
+     * i = 8は1を2回か2を1回 1を1回足した時点であと1だということは↑からわかる
+     * i = 7のとき1を3回 2を1回1を1回 i=8まで行った時点で2だということはわかる
+     */
+    int dfs(int n, int i) {
+        if (i >= n) return i == n ? 1 : 0;
+        if (cache[i] != -1) return cache[i];
+        return cache[i] = dfs(n, i + 1) + dfs(n, i + 2);
+    }
+
     public int climbStairs(int n) {
-        return myClimbStairs(n);
+        return dp(n);
+        // return myClimbStairs(n);
     }
 
     /**
@@ -13,18 +35,18 @@ class Solution {
      * その後メモ化する
      */
     public int myClimbStairs(int n) {
-        search(0, n);
+        bruteforce(0, n);
         return ans;
     }
 
-    private void search(int i, int n) {
+    private void bruteforce(int i, int n) {
         if (i == n) {
             ans++;
             return;
         } else if (i > n) {
             return;
         }
-        search(i + 1, n);
-        search(i + 2, n);
+        bruteforce(i + 1, n);
+        bruteforce(i + 2, n);
     }
 }
