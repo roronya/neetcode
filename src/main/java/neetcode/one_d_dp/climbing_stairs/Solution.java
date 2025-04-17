b5ca451c17c9bee6eb4635df99ec41966602a85e
roronya <roronya628@gmail.com>
2025-04-17 20:02:36 +0900

    [Climbing Stairs] bruteforce

--- src/main/java/neetcode/one_d_dp/climbing_stairs/Solution.java
+++ src/main/java/neetcode/one_d_dp/climbing_stairs/Solution.java
@@ -1,7 +1,30 @@
 package neetcode.one_d_dp.climbing_stairs;
 
 class Solution {
+    private int ans;
+
     public int climbStairs(int n) {
-        return 0;
+        return myClimbStairs(n);
+    }
+
+    /**
+     * 1 or 2
+     * backtrackで良さそう
+     * その後メモ化する
+     */
+    public int myClimbStairs(int n) {
+        search(0, n);
+        return ans;
+    }
+
+    private void search(int i, int n) {
+        if (i == n) {
+            ans++;
+            return;
+        } else if (i > n) {
+            return;
+        }
+        search(i + 1, n);
+        search(i + 2, n);
     }
 }
