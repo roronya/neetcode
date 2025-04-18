2169d737f4b9327d2c7e3a152e268842e5ed0106
roronya <roronya628@gmail.com>
2025-04-18 10:11:44 +0900

    [House Robber] guard

--- src/main/java/neetcode/one_d_dp/house_robber/Solution.java
+++ src/main/java/neetcode/one_d_dp/house_robber/Solution.java
@@ -17,6 +17,9 @@ class Solution {
      * returnするのは最後尾か最後尾の左隣でMax取れば良さそう
      */
     public int myRob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
         nums[2] = nums[0] + nums[2];
         for(int i = 3; i< nums.length; i++) {
             nums[i] = Math.max(nums[i-2], nums[i-3]) + nums[i];
