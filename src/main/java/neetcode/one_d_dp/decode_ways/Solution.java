2f3e0b5e93aed24247022db4ed9c86fdb060c5d6
roronya <roronya628@gmail.com>
2025-04-25 04:45:28 +0900

    [Decode Ways] bottomUpDp

--- src/main/java/neetcode/one_d_dp/decode_ways/Solution.java
+++ src/main/java/neetcode/one_d_dp/decode_ways/Solution.java
@@ -26,11 +26,12 @@ class Solution {
      * dp[i-1] = dp[i] + Xの形になるかな
      */
     public int myNumDecodings(String s) {
-        backtrack(s);
-        return ans;
+        return bottomUpDP(s);
+        // dfs(s);
+        // return ans;
     }
 
-    private void backtrack(String s) {
+    private void dfs(String s) {
         if (s.isEmpty()) {
             ans++;
             return;
@@ -38,16 +39,30 @@ class Solution {
 
         String subs = s.substring(0, 1);
         int num = Integer.parseInt(subs);
-        if (num == 0) {
-            return;
-        }
-        backtrack(s.substring(1));
+        if (num == 0) return;
+        dfs(s.substring(1));
 
         if (s.length() < 2) return;
         subs = s.substring(0, 2);
         num = Integer.parseInt(subs);
         if (num > 26) return;
-        backtrack(s.substring(2));
+        dfs(s.substring(2));
+    }
+
+    private int bottomUpDP(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
     }
 }
 
