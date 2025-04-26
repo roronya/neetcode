4bf559406e86bdde88bdc2ff1524212c4c535de4
roronya <roronya628@gmail.com>
2025-04-26 19:16:40 +0900

    [Coin Change] add

---
+++ src/main/java/neetcode/coin_change/Solution.java
@@ -0,0 +1,8 @@
+package neetcode.coin_change;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        return 0;
+    }
+}
+
---
+++ src/test/java/neetcode/coin_change/SolutionTest.java
@@ -0,0 +1,48 @@
+package neetcode.coin_change;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    /**
+     * Input: coins = [1,5,10], amount = 12
+     * <p>
+     * Output: 3
+     */
+    @Test
+    void example1() {
+        int[] coins = {1, 5, 10};
+        int amount = 12;
+        Solution solution = new Solution();
+        int actual = solution.coinChange(coins, amount);
+        Assertions.assertEquals(3, actual);
+    }
+
+    /**
+     * Input: coins = [2], amount = 3
+     * <p>
+     * Output: -1
+     */
+    @Test
+    void example2() {
+        int[] coins = {2};
+        int amount = 3;
+        Solution solution = new Solution();
+        int actual = solution.coinChange(coins, amount);
+        Assertions.assertEquals(-1, actual);
+    }
+
+    /**
+     * Input: coins = [1], amount = 0
+     * <p>
+     * Output: 0
+     */
+    @Test
+    void example3() {
+        int[] coins = {1};
+        int amount = 0;
+        Solution solution = new Solution();
+        int actual = solution.coinChange(coins, amount);
+        Assertions.assertEquals(0, actual);
+    }
+}
