2986d2f3a92cb8192c090eea3374a9a8e1f1197f
roronya <roronya628@gmail.com>
2025-05-27 09:16:31 +0900

    [Word Break] add

---
+++ src/main/java/neetcode/one_d_dp/word_break/Solution.java
@@ -0,0 +1,10 @@
+package neetcode.one_d_dp.word_break;
+
+import java.util.List;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
+
---
+++ src/test/java/neetcode/one_d_dp/word_break/SolutionTest.java
@@ -0,0 +1,53 @@
+package neetcode.one_d_dp.word_break;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class SolutionTest {
+    /**
+     * Input: s = "neetcode", wordDict = ["neet","code"]
+     * <p>
+     * Output: true
+     */
+    @Test
+    void example1() {
+        String s = "neetcode";
+        List<String> wordDict = List.of("neet", "code");
+
+        Solution solution = new Solution();
+        boolean actual = solution.wordBreak(s, wordDict);
+        Assertions.assertTrue(actual);
+    }
+
+    /**
+     * Input: s = "applepenapple", wordDict = ["apple","pen","ape"]
+     * <p>
+     * Output: true
+     */
+    @Test
+    void example2() {
+        String s = "applepenapple";
+        List<String> wordDict = List.of("apple", "pen", "ape");
+
+        Solution solution = new Solution();
+        boolean actual = solution.wordBreak(s, wordDict);
+        Assertions.assertTrue(actual);
+    }
+
+    /**
+     * Input: s = "catsincars", wordDict = ["cats","cat","sin","in","car"]
+     * <p>
+     * Output: false
+     */
+    @Test
+    void example3() {
+        String s = "catsincars";
+        List<String> wordDict = List.of("cats", "cat", "sin", "in", "car");
+
+        Solution solution = new Solution();
+        boolean actual = solution.wordBreak(s, wordDict);
+        Assertions.assertFalse(actual);
+    }
+}
